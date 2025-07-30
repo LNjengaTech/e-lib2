8a719d3584f59226a0cee1740af77e472e2fc56a
hackTivist <bildadsimiyu6@gmail.com>
2025-07-30 11:53:15 +0300

    feat: Get all book categories and display them

--- app/Http/Controllers/CatalogueController.php
+++ app/Http/Controllers/CatalogueController.php
@@ -10,11 +10,14 @@ class CatalogueController extends Controller
     //get all books
     public function index()
     {
+        //get categories column from catalogues table
+        $categories = Catalogue::select('category')->distinct()->pluck('category');
         //dd(request('search'));
         return view('library-catalogue', [
             'books' => Catalogue::latest()
                 ->filter(request(['search', 'tags', 'category']))
-                ->paginate(10)
+                ->paginate(10),
+            'categories' => $categories
         ]);
     }
 
