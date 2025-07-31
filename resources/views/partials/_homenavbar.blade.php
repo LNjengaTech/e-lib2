9b095c9a5dbaecf9f286b5c30a525d6da2306986
hackTivist <bildadsimiyu6@gmail.com>
2025-07-31 19:01:03 +0300

    styling: Modify navbar styling

--- resources/views/partials/_homenavbar.blade.php
+++ resources/views/partials/_homenavbar.blade.php
@@ -1,5 +1,5 @@
 <header
-    class=" w-full mx-auto bg-dark p-4 px-5 rounded mt-2  max-630px:mt-0 max-630px:rounded-none max-630px:p-5 font-mono shadow shadow-slate-500">
+    class=" w-full mx-auto bg-dark p-4 px-5 rounded mt-2  max-890px:mt-0 max-890px:rounded-none max-890px:p-5 font-mono shadow shadow-slate-500">
     <nav class="text-light flex justify-between items-center">
         <a href='/' class ="text-2xl max-600px:text-xl font-bitcount">
             {{-- <img src="{{ asset('images/logo.png') }}" class="w-[120px] h-[50px] border" /> --}}
@@ -7,36 +7,36 @@
         </a>
 
         <ul id="sidebar"
-            class="flex items-center font-bold max-630px:flex-col max-630px:items-start max-630px:rounded max-630px:shadow-md  max-630px:border-y max-630px:fixed max-630px:top-[15px] max-630px:right-1 max-630px:bg-dark max-630px:gap-5 max-630px:h-[210px] max-630px:p-2 max-630px:pt-14 max-200px:right-0 max-200px:top-[70px] max-630px:hidden z-[100]">
+            class="flex items-center font-bold max-890px:flex-col max-890px:items-start max-890px:rounded max-890px:shadow-md  max-890px:border-y max-890px:fixed max-890px:top-[15px] max-890px:right-1 max-890px:bg-dark max-890px:gap-5 max-890px:h-[250px] max-890px:p-2 max-890px:pt-14 max-200px:right-0 max-200px:top-[70px] max-890px:hidden z-[100]">
             <li
-                class="mx-6 max-1000px:mx-2 max-630px:mx-1 cursor-pointer hover:bg-light hover:text-dark transition-all rounded duration-300 ">
+                class="mx-6 max-1000px:mx-2 max-890px:mx-1 cursor-pointer hover:bg-light hover:text-dark transition-all rounded duration-300 ">
                 <a href="/library-catalogue" class="p-1">Library Catalogue</a>
             </li>
-            <hr class="bg-light/80 w-[1px] h-5 max-630px:hidden">
+            <hr class="bg-light/80 w-[1px] h-5 max-890px:hidden">
             </hr>
             <li
-                class="mx-6 max-1000px:mx-2 max-630px:mx-1  cursor-pointer hover:bg-light hover:text-dark transition-all  rounded duration-300">
+                class="mx-6 max-1000px:mx-2 max-890px:mx-1  cursor-pointer hover:bg-light hover:text-dark transition-all  rounded duration-300">
                 <a href="/exam-bank" class="p-1">Exam Bank</a>
             </li>
 
             @auth
-                <hr class="bg-light/80 w-[1px] h-5 max-630px:hidden">
+                <hr class="bg-light/80 w-[1px] h-5 max-890px:hidden">
                 </hr>
                 <li
-                    class="mx-6 max-1000px:mx-2 max-630px:mx-1  cursor-pointer hover:bg-light hover:text-dark transition-all  rounded duration-300">
+                    class="mx-6 max-1000px:mx-2 max-890px:mx-1  cursor-pointer hover:bg-light hover:text-dark transition-all  rounded duration-300">
                     <a href={{ Auth()->user()->utype === 'ADM' ? '/admin/dashboard' : 'user-dashboard' }} class="p-1">My
                         Dashboard</a>
                     {{--  $user->utype === 'ADM' ? '/admin/dashboard' : '/user-dashboard' --}}
                 </li>
             @endauth
             <a href="{{ route('login') }}"
-                class="bg-light text-dark p-2 rounded px-3 font-bold hover:bg-light/90 transition-all 630px:hidden mt-2">
+                class="bg-light text-dark p-2 rounded px-3 font-bold hover:bg-light/90 transition-all 890px:hidden mt-2">
                 <span>Login</span>
                 <i class="fa-solid fa-book-open">
                 </i></a>
         </ul>
         @auth
-            <form action="{{ route('logout') }}" method="POST" class="max-630px:hidden">
+            <form action="{{ route('logout') }}" method="POST" class="max-890px:hidden">
                 @csrf
                 <button type="submit"
                     class="bg-red-500 text-light p-2 rounded px-3 font-bold hover:bg-light/90 transition-all ">
@@ -45,7 +45,7 @@
                     </i></button>
             </form>
         @else
-            <section class="max-630px:hidden">
+            <section class="max-890px:hidden">
                 <a href="{{ route('login') }}"
                     class="bg-light text-dark p-2 rounded px-3 font-bold hover:bg-light/90 transition-all ">
                     <span>Login</span>
@@ -54,7 +54,7 @@
             </section>
         @endauth
 
-        <section class="630px:hidden z-[100]">
+        <section class="890px:hidden z-[100]">
             <i id="openSidebar" class="fa-solid fa-bars-staggered text-xl cursor-pointer"></i>
             <i id="closeSidebar" class="fa-solid fa-xmark text-2xl cursor-pointer hidden"></i>
         </section>
@@ -67,12 +67,12 @@
             const closeBtn = document.getElementById("closeSidebar");
             if (openBtn && closeBtn && sidebar) {
                 openBtn.addEventListener("click", function() {
-                    sidebar.classList.remove("max-630px:hidden");
+                    sidebar.classList.remove("max-890px:hidden");
                     openBtn.classList.add("hidden");
                     closeBtn.classList.remove("hidden");
                 });
                 closeBtn.addEventListener("click", function() {
-                    sidebar.classList.add("max-630px:hidden");
+                    sidebar.classList.add("max-890px:hidden");
                     openBtn.classList.remove("hidden");
                     closeBtn.classList.add("hidden");
                 });
